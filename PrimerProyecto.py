480ccf2aaa91ae08ec0614dfcb08cdcb9bcff058
jobarv <jbarrera@motiontech.com.mx>
2025-10-01 11:31:21 -0600

    DesdeLinux

---
+++ PrimerProyecto.py
@@ -0,0 +1,2 @@
+# Crea un programa que genere el nombre de una cerveza en base a dos palabras propuestas por el usuario.
+print(input("Dame el primer nombre: \n")+" "+input("Dame el segundo nombre: \n"))
