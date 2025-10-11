b80df6fb7e25689be344ae57460d5eb8206842ab
jobarv <jbarrera@motiontech.com.mx>
2025-10-11 00:36:57 -0600

    Cuarto proyecto

---
+++ CuartoProyecto.py
@@ -0,0 +1,31 @@
+# Juego de Adivina el numero
+from random import randint
+
+
+
+def jugar():
+     
+    
+    numeroUsuario = (int(input("Dame un numero entre 0 y 100: ")))
+
+    if 0 <= numeroUsuario <= 100:
+        numeroRandom = intento
+    else:
+        print("El numero no es valido")
+
+
+    numeroRandom = randint(0, 100)
+
+
+    if numeroRandom == intento:
+        print("Ganaste!")
+    
+    else: 
+        print("No Ganaste!")
+        
+for intento in range(8):
+    if jugar():
+        break
+else:
+    print("Lo siento, te quedaste sin oportunidades")
+            
