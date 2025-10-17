acdbc9fd6e81fdfe11f61d9e261f3a7aaa108cd3
jobarv <jbarrera@motiontech.com.mx>
2025-10-16 19:13:27 -0600

    Fin del Cuarto Proyecto 2

--- CuartoProyecto.py
+++ CuartoProyecto.py
@@ -1,32 +1,25 @@
 # Juego de Adivina el numero
 from random import randint
 
+for intento in range(8):
 
-
-def jugar():
-     
-    
     numeroUsuario = (int(input("Dame un numero entre 0 y 100: ")))
 
     if 0 <= numeroUsuario <= 100:
-        numeroRandom = intento
+    
+        numeroRandom = randint(0, 100)
+
     else:
         print("El numero no es valido")
 
-
-    numeroRandom = randint(0, 100)
-
-
-    if numeroRandom == intento:
+    if numeroRandom == numeroUsuario:
         print("Ganaste!")
-        print(f"El numero era {intento}.")
+        print(f"El numero era {numeroRandom}.")
     
     else: 
         print("No Ganaste!")
         
-for intento in range(8):
-    if jugar():
-        break
 else:
-    print("Lo siento, te quedaste sin oportunidades")
-            
+    print("Lo siento, te quedaste sin oportunidades.")
+    print(f"El numero era: {numeroRandom}")
+    
